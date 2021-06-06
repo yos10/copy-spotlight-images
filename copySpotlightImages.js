31c8077608687de34f2118361574b4b7dc9aa3f4
yos10 <61726023+yos10@users.noreply.github.com>
2021-06-06 13:48:40 +0900

    コメント追加など

--- copySpotlightImages.js
+++ copySpotlightImages.js
@@ -14,25 +14,34 @@ if (!fs.existsSync(destinationDir)) {
   fs.mkdirSync(destinationDir);
 }
 
-async function getFilePaths() {
+/**
+ * sourceDir 内で destinationDir に保存済みでないファイルのフルパスを配列で返す。
+ * @returns {array}
+ */
+function getFiles() {
   // 保存済みか確認するためファイルから末尾の拡張子を削除したファイル名の配列をつくる
   const savedFiles = fs.readdirSync(destinationDir).map(f => path.basename(f, '.jpg'));
   // 保存済みか調べる。そうでなければそれらが新規に追加するファイル
   const newFiles = fs.readdirSync(sourceDir).filter(f => savedFiles.indexOf(f) === -1);
-  const filePaths = newFiles.map(f => path.join(sourceDir, f));
+  const files = newFiles.map(f => path.join(sourceDir, f));
   // console.log(newFiles);
-  // console.log(filePaths);
-  return filePaths;
+  // console.log(files);
+  return files;
 }
 
-async function filterWallpaperImages(filePaths) {
+/**
+ * ファイルの種類が jpg かつ横幅が 1920 以上の壁紙用のファイルの配列を返す。
+ * @param {array} files 
+ * @returns {array}
+ */
+async function filterWallpaperImages(files) {
   const wallpaperImages = [];
-  for (const filePath of filePaths) {
-    const isJpg = await FileType.fromFile(filePath);
-    const imageWidth = sizeOf(filePath).width;
+  for (const file of files) {
+    const fileType = await FileType.fromFile(file);
+    const imageWidth = sizeOf(file).width;
     // sourceDir には壁紙用ではないファイルが存在しているので判別
-    if (isJpg.ext === 'jpg' && imageWidth >= 1920) {
-      wallpaperImages.push(filePath);
+    if (fileType.ext === 'jpg' && imageWidth >= 1920) {
+      wallpaperImages.push(file);
     }
   }
   // console.log(wallpaperImages);
@@ -40,14 +49,22 @@ async function filterWallpaperImages(filePaths) {
 }
 
 
-getFilePaths()
-  .then((filePaths) => filterWallpaperImages(filePaths))
-  .then((wallpaperImages) => {
-    wallpaperImages.forEach((image) => {
-      const fileName = path.basename(image);
-      const dest = path.join(destinationDir, `${fileName}.jpg`);
-      // console.log(dest);
-      fs.copyFileSync(image, dest);
-    })
-  })
-  .catch(console.error);
+
+function copyImages(wallpaperImages) {
+  for (const image of wallpaperImages) {
+    const fileName = path.basename(image);
+    const dest = path.join(destinationDir, `${fileName}.jpg`);
+    // console.log(fileName);
+    fs.copyFileSync(image, dest);
+  }
+}
+
+
+
+async function run() {
+  const files = getFiles();
+  const wallpaperImages = await filterWallpaperImages(files);
+  copyImages(wallpaperImages);
+}
+
+run();
